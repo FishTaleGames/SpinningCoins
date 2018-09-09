9c6bca2e3efcb5a62fd61933fb890358d332aa29
Austin Fisher <42326201+FishTaleGames@users.noreply.github.com>
2018-09-09 15:00:24 -0700

    Add files via upload

---
+++ PlayCoins.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayCoins : MonoBehaviour, IPointerClickHandler
+{
+    public ParticleSystem coins;
+
+    [Header("Ratio of Screen Width Radius")]
+    public float coinScale;
+
+
+    private void Start()
+    {
+        SetCoinsEmitterSize();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        PlayCoinsEmitter();
+    }
+
+
+    private void SetCoinsEmitterSize()
+    {
+        float screenWidth = transform.parent.GetComponent<RectTransform>().rect.width;
+        float newCoinScale = screenWidth * coinScale;
+        coins.GetComponent<RectTransform>().localScale = new Vector3(newCoinScale, newCoinScale, newCoinScale);
+        
+        ParticleSystem.ShapeModule coinShape = coins.shape;
+        coinShape.radius = (screenWidth / newCoinScale) / 2;
+    }
+
+    private void PlayCoinsEmitter()
+    {
+        if (!coins.isPlaying)
+            coins.Play();
+    }
+}
